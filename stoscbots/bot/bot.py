473bd4db2ac4244d41e97e35b07b382bcf514c9b
Vibin <327990+viper25@users.noreply.github.com>
2021-12-09 16:06:35 +0800

    Update bot.py

--- stoscbots/bot/bot.py
+++ stoscbots/bot/bot.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 from pyrogram import Client
 
-client = Client(session_name="stoscbot", api_id=os.environ.get('PYROGRAM_API_ID'), api_hash=os.environ.get('PYROGRAM_API_HASH'), device_model=os.environ.get('PYROGRAM_DEVICE_MODEL'), system_version=os.environ.get('PYROGRAM_SYSTEM_VERSION') bot_token=os.environ.get('STOSC_TELEGRAM_BOT_TOKEN'))
+client = Client(session_name="stoscbot", api_id=os.environ.get('PYROGRAM_API_ID'), api_hash=os.environ.get('PYROGRAM_API_HASH'), device_model=os.environ.get('PYROGRAM_DEVICE_MODEL'), system_version=os.environ.get('PYROGRAM_SYSTEM_VERSION'), bot_token=os.environ.get('STOSC_TELEGRAM_BOT_TOKEN'))
     
 def run():
     client.start_time = datetime.now()
