8032149e6b16cb845052bddedd014183744b96aa
viper25 <327990+viper25@users.noreply.github.com>
2022-02-19 19:39:34 +0800

    Add message length error msg
    If msg > 4096 don't send prayer list but send message saying so

--- plugins/handlers_services.py
+++ plugins/handlers_services.py
@@ -5,25 +5,27 @@ from stoscbots.bot import keyboards
 from dateutil import parser
 import gspread
 
-'''
+"""
 Handle multiple callback queries data and return filter for each
-'''
+"""
+
+
 def dynamic_data_filter(data):
     return filters.create(
-        lambda flt, _, query: query.data.startswith(flt.data),
-        data=data  # "data" kwarg is accessed with "flt.data" above
+        lambda flt, _, query: query.data.startswith(flt.data), data=data  # "data" kwarg is accessed with "flt.data" above
     )
+
 
 # --------------------------------------------------
 @Client.on_callback_query(dynamic_data_filter("Registrations for Service"))
 @loggers.log_access
 def get_service_registrations(client, query):
     query.answer()
-    _service_id = query.data.split(' ')[3]
-    serviceName, serviceDate, serviceList, kids_count=db.get_members_for_serviceID(_service_id)
-    msg=f"**{serviceName}** ({_service_id})\n`{serviceDate}`\n"
+    _service_id = query.data.split(" ")[3]
+    serviceName, serviceDate, serviceList, kids_count = db.get_members_for_serviceID(_service_id)
+    msg = f"**{serviceName}** ({_service_id})\n`{serviceDate}`\n"
     msg += "➖➖➖➖➖➖➖\n\n"
-    total=0
+    total = 0
     for _item in serviceList:
         # 33 is max char that shows on one line on mobile in this setting
         if len(f"`{_item[0]}:{_item[2]}` - {_item[1].strip()}") > 33:
@@ -39,6 +41,8 @@ def get_service_registrations(client, query):
         utils.edit_and_send_msg(query, msg, keyboards.get_services_keyboard(db.get_next_services()))
     else:
         utils.edit_and_send_msg(query, msg)
+
+
 # --------------------------------------------------
 @Client.on_callback_query(dynamic_data_filter("Prayer Requests"))
 @loggers.log_access
@@ -56,14 +60,20 @@ def get_prayer_requests(client, query):
     start_of_week = utils.week_start_from_service_time()
 
     # Check if there is any prayer request for this week submitted after current service starts at 7.45
-    prayer_requests_for_the_week = [_request for _request in list_requests if (parser.parse(_request[0]) - start_of_week).total_seconds() > 0]
-    departed_requests = [[_request[1],_request[2],_request[3]] for _request in prayer_requests_for_the_week if _request[3] != '']
-    sick_requests = [[_request[1],_request[2],_request[4]] for _request in prayer_requests_for_the_week if _request[4] != '']
-    blessings_requests = [[_request[1],_request[2],_request[5]] for _request in prayer_requests_for_the_week if _request[5] != '']
+    prayer_requests_for_the_week = [
+        _request for _request in list_requests if (parser.parse(_request[0]) - start_of_week).total_seconds() > 0
+    ]
+    departed_requests = [
+        [_request[1], _request[2], _request[3]] for _request in prayer_requests_for_the_week if _request[3] != ""
+    ]
+    sick_requests = [[_request[1], _request[2], _request[4]] for _request in prayer_requests_for_the_week if _request[4] != ""]
+    blessings_requests = [
+        [_request[1], _request[2], _request[5]] for _request in prayer_requests_for_the_week if _request[5] != ""
+    ]
 
     if prayer_requests_for_the_week:
         msg = f"**Prayer Requests**\n`since {start_of_week.strftime('%b %d, %H:%M %p')}`\n"
-        msg+= "➖➖➖➖➖➖➖➖\n"
+        msg += "➖➖➖➖➖➖➖➖\n"
 
         if departed_requests:
             msg += "\n--The Departed--"
@@ -95,6 +105,13 @@ def get_prayer_requests(client, query):
                     # Only 1 name in the list
                     msg += f"\n• {_request[2]}"
 
-        utils.edit_and_send_msg(query, msg, keyboards.get_services_keyboard(db.get_next_services()))
+        if len(msg) >= 4096:
+            utils.edit_and_send_msg(
+                query,
+                "List too Long. Please check Google Sheet for full list.",
+                keyboards.get_services_keyboard(db.get_next_services()),
+            )
+        else:
+            utils.edit_and_send_msg(query, msg, keyboards.get_services_keyboard(db.get_next_services()))
     else:
         utils.edit_and_send_msg(query, "No Prayer Requests yet", keyboards.get_services_keyboard(db.get_next_services()))
