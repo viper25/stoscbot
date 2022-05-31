6445ca14cb819d6523b3c1470a5dd943d8d0e9de
viper25 <327990+viper25@users.noreply.github.com>
2022-05-31 17:32:33 +0800

    Add Bday & Anniversary years
    Also highlight todays bdays and anniversaries in weekly list

--- plugins/handlers_members.py
+++ plugins/handlers_members.py
@@ -1,3 +1,4 @@
+from datetime import date
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery
 from stoscbots.bot import keyboards
@@ -25,7 +26,13 @@ async def get_today_bdays(client: Client, query: CallbackQuery):
     elif len(result) > 0:
         msg="**Birthdays today** 🎂\n\n"
         for _item in result:
-            msg += f"• {_item[1].strip()} `({_item[0].strip()})`\n"
+            _splits = _item[2].split('/')
+            # Not all members provide year, therefore check if present
+            if len(_splits) == 3:
+                age =  date.today().year - int(_splits[2])
+            else:
+                age = None
+            msg += f"• {_item[1].strip()} `({_item[0].strip()})` - {age if age else 'NA'}\n"
     await utils.edit_and_send_msg(query, msg, keyboards.members_menu_keyboard)
 # --------------------------------------------------
 @Client.on_callback_query(dynamic_data_filter("Member Anniversary Today Button"))
@@ -38,7 +45,8 @@ async def get_today_anniversaries(client: Client, query: CallbackQuery):
     else:
         msg="**Wedding Anniversarys today** 💍\n\n"
         for _item in result:
-            msg += f"• {_item[1].strip()} `({_item[0].strip()})`\n"
+            anniversary_years =  date.today().year - _item[2].year
+            msg += f"• {_item[1].strip()} `({_item[0].strip()})` - {anniversary_years if anniversary_years else 'NA'}\n"
     await utils.edit_and_send_msg(query, msg, keyboards.members_menu_keyboard)
 # --------------------------------------------------
 @Client.on_callback_query(dynamic_data_filter("Member Birthday Week Button"))
@@ -52,7 +60,18 @@ async def get_weeks_bdays(client: Client, query: CallbackQuery):
         msg="**Birthdays this week** 🎂\n"
         msg += f"`({start} - {end})`\n\n"
         for _item in result:
-            msg += f"• {_item[1].strip()} `({_item[0].strip()})`\n"
+            _splits = _item[2].split('/')
+            _day = _splits[0].zfill(2)
+            _month = _splits[1].zfill(2)
+            # Not all members provide year, therefore check if present
+            if len(_splits) == 3:
+                age =  date.today().year - int(_splits[2])
+            else:
+                age = None
+            if  _day == date.today().strftime("%d") and _month == date.today().strftime("%m") :
+                msg += f"•** {_item[1].strip()}** `({_item[0].strip()})` - **{age if age else 'NA'}**\n"
+            else:
+                msg += f"• {_item[1].strip()} `({_item[0].strip()})` - {age if age else 'NA'}\n"
     await utils.edit_and_send_msg(query, msg, keyboards.members_menu_keyboard)
 # --------------------------------------------------
 @Client.on_callback_query(dynamic_data_filter("Member Anniversary Week Button"))
@@ -66,7 +85,11 @@ async def get_weeks_anniversaries(client: Client, query: CallbackQuery):
         msg="**Wedding Anniversary** 💍\n"
         msg += f"`({start} - {end})`\n\n"
         for _item in result:
-            msg += f"• {_item[1].strip()} `({_item[0].strip()})`\n"
+            anniversary_years =  date.today().year - _item[2].year
+            if str(_item[2].day) == date.today().strftime("%d") and _item[2].month == date.today().strftime("%m") :
+                msg += f"•** {_item[1].strip()}** `({_item[0].strip()})` - **{anniversary_years if anniversary_years else 'NA'}**\n"
+            else:
+                msg += f"• {_item[1].strip()} `({_item[0].strip()})` - {anniversary_years if anniversary_years else 'NA'}\n"
     await utils.edit_and_send_msg(query, msg, keyboards.members_menu_keyboard)
 # --------------------------------------------------
 @Client.on_callback_query(dynamic_data_filter("GB Ineligible"))
