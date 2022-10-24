8082e6ec0a0110c400e0ddee02dc96f81a692324
viper25 <327990+viper25@users.noreply.github.com>
2022-10-24 19:16:06 +0800

    Add Versioning and Cache to GitHub Actions
    * Add the ability to dynamically create versions from GitHub Actions
    * Use GitHub Actions cache to speed up build so each job doesn't have to do git pulls.

--- plugins/handlers_dev.py
+++ plugins/handlers_dev.py
@@ -1,13 +1,20 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from stoscbots.util import loggers, bot_auth
+import configparser
 
-# ==================================================
-# Command Handlers
 @Client.on_message(filters.command(["version","ver"]))
 @loggers.async_log_access
 @bot_auth.async_management_only
 async def version_handler(client: Client, message: Message):
-    msg = f"Version: **{message._client.APP_VERSION}** on **{message._client.SYSTEM_VERSION}**"
-    msg += f"\nSession Name: **{client.name}**"
+
+    config = configparser.ConfigParser()
+    config.read(r'.VERSION')
+    msg = f"Telegram Version: **{message._client.APP_VERSION}** on **{message._client.SYSTEM_VERSION}**"
+    msg += f"\nSession Name: **{client.name}**\n"
+    msg += "-------------"
+    msg += f"\nRelease Version: **{config.get('version', 'VERSION')}**"
+    msg += f"\nBuild SHA: **{config.get('version', 'BUILD')[:7]}**"
+    msg += f"\nBranch: **{config.get('version', 'BRANCH')}**"
+    
     await message.reply_text(msg)
