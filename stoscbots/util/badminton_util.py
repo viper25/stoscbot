a1a8222b20d10cc7c2f773790f9232591eb8b650
viper25 <327990+viper25@users.noreply.github.com>
2023-09-26 11:42:33 +0800

    Update

--- stoscbots/util/badminton_util.py
+++ stoscbots/util/badminton_util.py
@@ -2,6 +2,8 @@ import random
 from collections import Counter
 from itertools import combinations
 
+from stoscbots.util.image import create_schedule_image
+
 
 def generate_badminton_doubles_schedule(player_names: list[str], num_matches: int):
     # Ensure there are at least 4 players to schedule a doubles match.
@@ -31,7 +33,6 @@ def generate_badminton_doubles_schedule(player_names: list[str], num_matches: in
         # This is done to prioritize matches with players who've played the least.
         min_match_count = min(
             sum(player_played_match_counter[player] for player in m[0] + m[1]) for m in possible_matches)
-
 
         for match in possible_matches:
             team1, team2 = match
@@ -74,14 +75,15 @@ if __name__ == "__main__":
     player_names = ["Anub", "Jubin", "Simon", "Ajsh", "Vinct", "Liju", "Jithin", "Prdip", "Vibin", "Dibu"]
     schedule, player_count = generate_badminton_doubles_schedule(player_names, num_matches)
 
-    # img = create_schedule_image(schedule, player_count, player_names)
-
     from tabulate import tabulate
 
     # Display the schedule in table format
     table_data = []
     for i, match in enumerate(schedule):
-        table_data.append([i + 1, '|'.join(match[0]), '|'.join(match[1])])
+        table_data.append([i + 1, ', '.join([item for sublist in match for item in sublist])])
+
+    # img = create_schedule_image(table_data, player_count, player_names)
+
     print(tabulate(table_data, headers=["#", "Team 1", "Team 2"], tablefmt="simple"))  # Good
     print(tabulate(table_data, headers=["#", "Team 1", "Team 2"], tablefmt="rst"))  # Good
     print(tabulate(table_data, headers=["#", "Team 1", "Team 2"], tablefmt="presto",
