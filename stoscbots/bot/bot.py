5a7735bbc0e260f4aadeb6562305b76d00f6979a
viper25 <327990+viper25@users.noreply.github.com>
2023-11-03 11:29:19 +0800

    Add startup banner

--- stoscbots/bot/bot.py
+++ stoscbots/bot/bot.py
@@ -1,9 +1,11 @@
+import logging
 import os
 from datetime import datetime
-from pyrogram import Client
-from stoscbots.util.loggers import LOGLEVEL
-import logging
+
 from convopyro import Conversation
+from pyrogram import Client
+
+from stoscbots.util.loggers import LOGLEVEL
 
 # ------------------------------------------------------------------
 # Module logger
@@ -14,11 +16,33 @@ logger.info(f"Log Level: {LOGLEVEL}. Set in Environment Variable LOGLEVEL")
 
 test_mode = True
 ENV = os.environ.get("ENV")
+env_banner = """
+    ▀▀█▀▀ █▀▀ █▀▀ ▀▀█▀▀ 
+    ░▒█░░ █▀▀ ▀▀█ ░░█░░ 
+    ░▒█░░ ▀▀▀ ▀▀▀ ░░▀░░
+    """
 if ENV == "PRO":
     test_mode = False
+    env_banner = """
+    ▒█▀▀█ █▀▀█ █▀▀█ █▀▀▄ █░░█ █▀▀ ▀▀█▀▀ ░▀░ █▀▀█ █▀▀▄ 
+    ▒█▄▄█ █▄▄▀ █░░█ █░░█ █░░█ █░░ ░░█░░ ▀█▀ █░░█ █░░█ 
+    ▒█░░░ ▀░▀▀ ▀▀▀▀ ▀▀▀░ ░▀▀▀ ▀▀▀ ░░▀░░ ▀▀▀ ▀▀▀▀ ▀░░▀
+    """
+
+print("""
+░██████╗████████╗░█████╗░░██████╗░█████╗░  ██████╗░░█████╗░████████╗
+██╔════╝╚══██╔══╝██╔══██╗██╔════╝██╔══██╗  ██╔══██╗██╔══██╗╚══██╔══╝
+╚█████╗░░░░██║░░░██║░░██║╚█████╗░██║░░╚═╝  ██████╦╝██║░░██║░░░██║░░░
+░╚═══██╗░░░██║░░░██║░░██║░╚═══██╗██║░░██╗  ██╔══██╗██║░░██║░░░██║░░░
+██████╔╝░░░██║░░░╚█████╔╝██████╔╝╚█████╔╝  ██████╦╝╚█████╔╝░░░██║░░░
+╚═════╝░░░░╚═╝░░░░╚════╝░╚═════╝░░╚════╝░  ╚═════╝░░╚════╝░░░░╚═╝░░░
+"""
+      )
+print(f"Environment:\n{env_banner}")
 
 app = Client(os.environ.get("BOT_CLIENT_NAME"), plugins=dict(root="plugins"), test_mode=test_mode)
 Conversation(app)
+
 
 def run():
     app.start_time = datetime.now()
